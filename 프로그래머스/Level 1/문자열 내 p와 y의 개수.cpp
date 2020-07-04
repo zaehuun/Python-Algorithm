3c68dda61063a29b41d283f30fc765b59decfaf6
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-07-05 06:17:32 +0900

    gap 변수 하나 두어 해결

---
+++ 프로그래머스/Level 1/문자열 내 p와 y의 개수.cpp
@@ -0,0 +1,29 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12916
+//나는 이런 게 재밌네
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int gap = 'a' - 'A';
+    int len = s.length();
+    int pcnt = 0;
+    int ycnt = 0;
+    for(int i = 0; i < len; i++){
+        if(s[i] == 'p' || s[i] == ('p' - gap) ){
+            pcnt++;
+        }
+        if(s[i] == 'y' || s[i] == ('y' - gap) ){
+            ycnt++;
+        }
+    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+    if(((ycnt == 0) && (pcnt == 0)) || (ycnt == pcnt) )
+        answer = true;
+    else if (ycnt != pcnt)
+        answer = false;
+    return answer;
+}
