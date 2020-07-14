401d04b193cf5c124a3c826f71a1a772353692c6
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-07-15 04:30:34 +0900

    최대 공약수, 최소 공배수

---
+++ 프로그래머스/Level 1/최대공약수와 최소공배수.cpp
@@ -0,0 +1,25 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12940
+
+#include <string>
+#include <vector>
+
+using namespace std;
+int gcd(int a, int b){
+	int c;
+	while (b != 0){
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int lcm(int a, int b){
+    return a * b / gcd(a, b);
+}
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    answer.push_back(gcd(n,m));
+    answer.push_back(lcm(n,m));
+    return answer;
+}
