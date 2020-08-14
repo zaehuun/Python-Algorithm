7106a027bcc59e36a158fafa871cb1e1991b94b2
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-08-14 23:22:25 +0900

    and 연산

---
+++ 프로그래머스/Level 1/짝수와 홀수.py
@@ -0,0 +1,7 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12937?language=python3
+
+def solution(num):
+    answer = ''
+    if(num & 1) :answer = "Odd"
+    else :answer = "Even"
+    return answer
