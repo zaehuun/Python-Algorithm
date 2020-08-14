3da9d6a971467624533a27d4ffcf12b79b6ed7c5
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-08-14 23:45:56 +0900

    Create 핸드폰 번호 가리기.py

---
+++ 프로그래머스/Level 1/핸드폰 번호 가리기.py
@@ -0,0 +1,5 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12948?language=python3
+
+def solution(phone_number):
+    answer = '*' * (len(phone_number)-4) + phone_number[-4:]
+    return answer
