b3ac9bae4a2f848c13985efff2421f1c39248127
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-08-17 23:09:03 +0900

    Create 예산.py

---
+++ 프로그래머스/Summer,Winter Coding(~2018)/예산.py
@@ -0,0 +1,12 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12982?language=python3
+
+def solution(d, budget):
+    answer = 0
+    d.sort()
+    
+    for i in d:
+        budget -= i
+        if budget < 0:
+            break
+        answer+=1
+    return answer
