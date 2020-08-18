9e07e604390bad2e4a97034c0f834391838f07e2
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-08-19 00:31:35 +0900

    Create 전화번호 목록.py

---
+++ 프로그래머스/Level 2/전화번호 목록.py
@@ -0,0 +1,11 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42577?language=python3
+
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    
+    for i in range(0, len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            return False
+    
+    return answer
