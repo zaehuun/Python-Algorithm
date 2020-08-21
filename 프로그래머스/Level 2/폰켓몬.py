e99a1d80f3b71341926c583fccaf94e086ecd202
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-08-22 05:16:29 +0900

    Update 폰켓몬.py

--- 프로그래머스/Level 2/폰켓몬.py
+++ 프로그래머스/Level 2/폰켓몬.py
@@ -1,19 +1,13 @@
 //https://programmers.co.kr/learn/courses/30/lessons/1845
 
 def solution(nums):
-    answer = 0
 
     pick = len(nums)//2
     
-    nums = list(set(nums))
+    nums = len(set(nums))
     
-    tmp = 1
-    for i in range(0, len(nums)-1):
-        if nums[i] < nums[i+1]:
-            tmp+=1
-            
-    if tmp > pick:
+    if nums > pick:
         return pick
     else:
-        return tmp
+        return nums
         
