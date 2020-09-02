38f5ec1b77c0b7f637a86b4922efc0096e716b60
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-09-03 00:33:03 +0900

    Create 점프와 순간 이동.py

---
+++ 프로그래머스/Summer,Winter Coding(~2018)/점프와 순간 이동.py
@@ -0,0 +1,12 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12980
+
+def solution(n):
+    ans = 0
+    
+    while n:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n - 1
+            ans += 1
+    return ans
