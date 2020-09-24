2a2d6ea384b9f7e9821877f96c81d4301009cfa5
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-09-25 04:09:39 +0900

    iter

---
+++ 백준/이진 탐색/[실전 문제] 떡볶이 떡 만들기.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+line = list(map(int,input().split()))
+st = 0
+end = max(line)
+
+answer = 0
+while st <= end:
+    mid = (st + end) //2
+    total = 0
+    for i in line:
+        if i > mid:
+            total = total + i - mid
+
+    if total == m:
+        answer = total
+        st += 1
+    elif total > m:
+        st += 1
+    else: #total < m
+        end -= 1
+
+print(answer)
+
