5f13771b00a01bd6a32b5bbf5b47296c91d95df6
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-03-03 00:06:38 +0900

    아오 복잡

---
+++ 프로그래머스/2021 KAKAO BLIND RECRUITMENT/메뉴 리뉴얼.py
@@ -0,0 +1,23 @@
+#https://programmers.co.kr/learn/courses/30/lessons/72411
+
+import itertools
+from collections import Counter
+def solution(orders, course):
+
+    answer = []
+    #print(sorted(orders))
+    for i in course:
+        arr = []
+        for j in orders:
+            print(sorted(j))
+            t = itertools.combinations(sorted(j),i)
+            arr += t
+
+        counter = Counter(arr)
+
+        if  len(counter) != 0 and max(counter.values()) != 1:
+            answer = answer + [''.join(i) for i in counter if counter[i] == max(counter.values())]
+        #print(answer)
+    #print(answer)
+    print(answer)
+    return sorted(answer)
