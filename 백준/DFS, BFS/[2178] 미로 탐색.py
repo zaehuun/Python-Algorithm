9e8d4688d9615eed4315dfe95519b035b9047d3f
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-03-31 01:06:48 +0900

    BFS - 코드 더 짧게

--- 백준/DFS, BFS/[2178] 미로 탐색.py
+++ 백준/DFS, BFS/[2178] 미로 탐색.py
@@ -1,39 +1,24 @@
-//https://www.acmicpc.net/problem/2178
+#https://www.acmicpc.net/problem/2178
 
 from collections import deque
-queue = deque()
-N, M = map(int, input().split())
 
-arr = [[] for _ in range(N)]
-check = [[False] * M for _ in range(N)]
+N, M = map(int,input().split())
 
-for i in range(N):
-    s = input()
-    for j in s:
-        arr[i].append(int(j))
+arr = [list(input()) for _ in range(N)]
 
-dx = (1, -1, 0, 0)
-dy = (0, 0, 1, -1)
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
 
-def bfs(x,y):
-    check[x][y] = True
-    arr[x][y] = 1
-    queue.append((x,y,1))
-
-    while queue:
-        qx, qy, cnt = queue.popleft()
-        if qx == N-1 and qy == M-1:
-            return cnt
-        for i in range(4):
-            tx = qx + dx[i]
-            ty = qy + dy[i]
-
-            if tx < 0 or tx >= N or ty < 0 or ty >= M or arr[tx][ty] == 0:
-                continue
-            if check[tx][ty] == True:
-                continue
-            check[tx][ty] = True
+queue = deque([(0,0,1)])
+while queue:
+    x, y, cnt = queue.popleft()
+    if x == N - 1 and y == M - 1:
+        print(cnt)
+        break
+    for i in range(4):
+        tx = x + dx[i]
+        ty = y + dy[i]
+        
+        if -1 < tx < N and -1 < ty < M and arr[tx][ty] == '1':
+            arr[tx][ty] = 0
             queue.append((tx,ty,cnt+1))
-
-print(bfs(0,0))
-
