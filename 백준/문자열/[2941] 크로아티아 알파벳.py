8ebf68ff3a18aa4916b6dc39ae3637ca21b6f08b
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-04-05 22:40:18 +0900

    다시 풀기

--- 백준/문자열/[2941] 크로아티아 알파벳.py
+++ 백준/문자열/[2941] 크로아티아 알파벳.py
@@ -2,13 +2,12 @@
 //매우 간단한 문제였는데 답을 보고 띠용..
 //index 에러 날까봐 걱정했는데 이렇게 풀면 전혀 걱정할게없었다..
 
-dic = ["c=","c-","dz=","d-","lj","nj","s=","z="]
+S = input()
 
-croa = input()
+alph = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+for s in alph:
+    if s in S:
+        S = S.replace(s,'*')
 
-for i in dic:
-    if i in croa:
-        croa = croa.replace(i,"*")
-
-
-print(len(croa))
+print(len(S))
+ 
