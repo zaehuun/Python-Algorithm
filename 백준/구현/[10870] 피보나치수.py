43c4bf40aae297da3b4c3e2ebede2312aaf5daa5
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-04-07 05:53:33 +0900

    재귀

---
+++ 백준/구현/[10870] 피보나치수.py
@@ -0,0 +1,12 @@
+#https://www.acmicpc.net/problem/10870
+n = int(input())
+
+
+def fib(n):
+    if n == 1:
+        return 1
+    if n == 0:
+        return 0
+    return fib(n-1) + fib(n-2)
+
+print(fib(n))
