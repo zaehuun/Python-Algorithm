cbc15de1546f2bc33dd0c691f7e6071829f3c03e
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-04-20 22:56:36 +0900

    원형 큐 - K번째 수 계쏙 제거해야 할 때

--- 백준/Queue, Deque/[11866] 요세푸스 문제 0.py
+++ 백준/Queue, Deque/[11866] 요세푸스 문제 0.py
@@ -1,6 +1,7 @@
 #https://www.acmicpc.net/problem/11866
 #1년 전에 어떻게 풀 지 몰라서 넘겼는데 좀만 생각해보니 쉽게 풀 수 있었다.
 #물론 코드가 효율적인지는 잘 모르겠다.
+#K번째 수를 어떻게 계속 처리할까했는데 원형 큐를 생각하니 됐다.
 
 from collections import deque
 
