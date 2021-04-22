58b1458430bf77b8391a2077a3ebc9f269e71efc
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-04-22 23:58:01 +0900

    스택

--- 백준/Stack/[12789] 도키도키 간식드리미.py
+++ 백준/Stack/[12789] 도키도키 간식드리미.py
@@ -1,5 +1,6 @@
 #https://www.acmicpc.net/problem/12789
 
+#처음 코드
 N = int(input())
 
 line = list(map(int,input().split()))
@@ -8,6 +9,31 @@ wait = []
 
 num = 1
 
+while line:
+    if line[0] == num:
+        line.pop(0)
+        num += 1
+    else:
+        while line and num != line[0]:
+            wait.append(line.pop(0))
+    while wait and wait[-1] == num:
+        wait.pop()
+        num += 1
+
+if not wait:
+    print("Nice")
+else:
+    print("Sad")
+    
+    
+    
+   
+#인터넷 보고 고친 코드
+
+N = int(input())
+line = list(map(int,input().split()))
+wait = []
+num = 1
 while line:
     v = line.pop(0)
     if v == num:
@@ -18,7 +44,6 @@ while line:
     while wait and wait[-1] == num:
         wait.pop()
         num += 1
-
 if not wait:
     print("Nice")
 else:
