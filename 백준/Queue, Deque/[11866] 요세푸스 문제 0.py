d5a742b9745a80039cec0b86f0a538af2f644d4b
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-07-29 02:06:26 +0900

    Update [11866] 요세푸스 문제 0.py

--- 백준/Queue, Deque/[11866] 요세푸스 문제 0.py
+++ 백준/Queue, Deque/[11866] 요세푸스 문제 0.py
@@ -3,22 +3,28 @@
 #물론 코드가 효율적인지는 잘 모르겠다.
 #K번째 수를 어떻게 계속 처리할까했는데 원형 큐를 생각하니 됐다.
 
+# from collections import deque
+# N, K = map(int,input().split())
+# idx = 1
+# deq = deque()
+# for i in range(N):
+#     deq.append(i+1)
+# answer = []
+# while deq:
+#     if idx == K:
+#         answer.append(deq.popleft())
+#         idx = 1
+#     else:
+#         deq.append(deq.popleft())
+#         idx += 1
+# print('<'+", ".join(map(str,answer))+'>')
+
+# 추천 문제에 있어서 다시 풀었는데 전보다 깔끔해졌다.
 from collections import deque
-
 N, K = map(int,input().split())
-
-idx = 1
-
-deq = deque()
-for i in range(N):
-    deq.append(i+1)
+deq = deque([i+1 for i in range(N)])
 answer = []
 while deq:
-    if idx == K:
-        answer.append(deq.popleft())
-        idx = 1
-    else:
-        deq.append(deq.popleft())
-        idx += 1
-
+    deq.rotate(-K+1)
+    answer.append(deq.popleft())
 print('<'+", ".join(map(str,answer))+'>')
