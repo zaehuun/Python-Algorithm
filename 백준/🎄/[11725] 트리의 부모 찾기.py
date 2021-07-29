d100e9df55d08208212bd9db0aed25f7f9863010
zaehuun <51132077+zaehuun@users.noreply.github.com>
2021-07-30 03:18:42 +0900

    Update [11725] 트리의 부모 찾기.py

--- 백준/🎄/[11725] 트리의 부모 찾기.py
+++ 백준/🎄/[11725] 트리의 부모 찾기.py
@@ -1,6 +1,9 @@
+import sys
+sys.setrecursionlimit(10 ** 9)
+
 N = int(input())
 answer = [0] * (N+1)
-arr = [[] * (N+1) for _ in range(N+1)]
+arr = [[] for _ in range(N+1)]
 def dfs(root):
     for i in arr[root]:
         if answer[i] == 0:  
@@ -11,7 +14,7 @@ for _ in range(N-1):
     arr[p].append(c)
     arr[c].append(p)
 dfs(1)
-for i in range(2,len(answer)):
+for i in range(2,N+1):
     print(answer[i])
 
 #       1
